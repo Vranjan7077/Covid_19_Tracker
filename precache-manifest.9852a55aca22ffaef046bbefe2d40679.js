53ef0c0ef703079240e188e58664d92642e2db5d
Vranjan7077 <ranvinay123@gmail.com>
2020-05-21 03:19:11 +0530

    First deploy with all countries

---
+++ precache-manifest.9852a55aca22ffaef046bbefe2d40679.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "079db23cb75f0fddb4535ca91de2d21d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d0406a2728ec2e258c7b",
+    "url": "/static/css/main.5029428f.chunk.css"
+  },
+  {
+    "revision": "7fa1a0cecb20a7598cfb",
+    "url": "/static/js/2.0e42d7b4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.0e42d7b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0406a2728ec2e258c7b",
+    "url": "/static/js/main.88ec286a.chunk.js"
+  },
+  {
+    "revision": "b16a71782802916e8eeb",
+    "url": "/static/js/runtime-main.f5f36e65.js"
+  },
+  {
+    "revision": "000143257b9c06ef53df2887705e124e",
+    "url": "/static/media/image.00014325.png"
+  }
+]);
